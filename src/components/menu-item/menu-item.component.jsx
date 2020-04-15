2b4a2b3810cfb20ffd315a6d9784c1eaa6857bc7
drinkcoffee <dcoffie92@gmail.com>
2020-04-15 13:08:32 -0700

    broke homepage down into smaller components, finished styling menu-item component

---
+++ src/components/menu-item/menu-item.component.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './menu-item.styles.scss'
+
+export const MenuItem = ({ title, imageUrl, size, linkUrl }) => {
+    return (
+        <div className={`${size} menu-item`}>
+            <div
+                className='background-image'
+                style={{
+                    backgroundImage: `url(${imageUrl})`
+                }}
+            >
+            </div>
+            <div className="content">
+                <h1 className="title">{title.toUpperCase()}</h1>
+                <span className="subTitle">{linkUrl}</span>
+            </div>
+        </div>
+    )
+}
---
+++ src/pages/homepage/Homepage.component.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Directory from '../../components/directory/directory.component'
+
+export const Homepage = () => {
+    return (
+        <div className='homepage'>
+            <Directory />
+        </div>
+    )
+}
